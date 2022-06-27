890680cbb0f640a64f62c145a8bce3736c8bc099
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-27 11:48:26 +0530

    Create DailyTask.js

---
+++ DailyTask.js
@@ -0,0 +1,39 @@
+// 1. Find output of 10+20+"30"
+console.log(10+20+'30') //3030
+
+// 2. Find output of "10"+20+30
+console.log('10'+20+30) //1050
+
+// 3. 10=='10' 
+console.log(10=='10') //boolean value True
+
+// 4. 20=== "20"
+console.log(20=='20') //boolean value False
+
+// 5. JS advantage
+Ans :  Easy to understand and learn,
+Rich interfaces can create a catchy webpages,
+Drag and drop components or slider may give the rich interfaces to the web page.
+
+// 6. For in and for each difference
+
+Ans : For loop will iterate through the given the number of times and forEach will iterate through elements 
+
+// 7. What is closure?
+
+Ans: Closure can give access outer functions scope from inner functions. 
+
+// 8.Write 5 math inbuilt method
+Math.ceil(4.1)// 5 is the OUTPUT  // it will give the nearest value positive and negative integer
+
+Math.floor(4.9)// 4
+
+Math.trunc(4.9)// 4  //returns the integer part like 4.9 is 4 output
+
+Math.abs(-4.7)// 4.7 // give the absolute value
+
+Math.min()// return the smallest value
+
+Math.max() // return the largest value
+
+Math.random()// 0 to 1 return the value 0 to 1
